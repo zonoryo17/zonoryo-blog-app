07fcfcd2bbd72e5dbf94153f395f164068ecb961
zonoryo17 <p.naka.0p0@gmail.com>
2023-12-17 09:14:34 +0900

    fix: 文言の修正

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,13 +1,16 @@
-// ディレクトリごとにオブジェクトを作成する
+// ディレクトリ（ドメイン）ごとにオブジェクトを作成する
 const BASE = {
   home: '/',
 } as const satisfies Record<string, `/${string}`>;
 
 const ARTICLES = {
   articleDetail: (id) => `/articles/${id}`,
-} as const satisfies Record<string, (id: string) => `/articles/${string}`>;
+} as const satisfies Record<
+  string,
+  `/articles/${string}` | ((id: string) => `/articles/${string}`)
+>;
 
-// ディレクトリごとのオブジェクトをpath内でマージする
+// ディレクトリ（ドメイン）ごとのオブジェクトをpath内でマージする
 export const path = {
   ...BASE,
   ...ARTICLES,
